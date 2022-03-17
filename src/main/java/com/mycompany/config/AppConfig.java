7e3c78a457615b07a418dc3306d869e08822d016
Sen <hoangnhan26212@gmail.com>
2022-03-18 00:44:30 +0700

    change information application

--- src/main/java/com/mycompany/config/AppConfig.java
+++ src/main/java/com/mycompany/config/AppConfig.java
@@ -17,8 +17,6 @@ import org.apache.velocity.exception.VelocityException;
 import org.hibernate.SessionFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
